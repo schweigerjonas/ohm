a96ff3045daf815afb2c8c750de05aecd0b36e8e
Jonas Schweiger <jonas.schweiger91@gmail.com>
2025-05-17 22:41:30 +0200

    Add bottom navigation bar and remove app bar

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,12 +10,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  // TODO: contains all pages the user can navigate to from the bottom nav bar
+  final List<Widget> _pages = [];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.show_chart), label: 'Statistics'),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add'),
+          BottomNavigationBarItem(icon: Icon(Icons.receipt_long), label: 'Entries')
+        ],
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
